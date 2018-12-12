2d4765403f530fd5f6530ea49efc303ffffd6e32
HHkm <jobwork.km778@gmail.com>
2018-12-12 23:24:51 +0900

    ベースアップ

---
+++ Source/app/src/main/java/com/example/samonapplication/ConfigActivity.java
@@ -0,0 +1,14 @@
+package com.example.samonapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
---
+++ Source/app/src/main/java/com/example/samonapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.samonapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        findViewById(R.id.button).setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    public void OnDetail(View view) {
+        Toast toast = Toast.makeText(this,"うどんとそば",Toast.LENGTH_SHORT);
+        toast.show();
+        goToDetailPage();
+    }
+
+    private void goToDetailPage() {
+        Intent intent;
+        intent = new Intent(getApplicationContext(),DetailActivity.class);
+        startActivity(intent);
+    }
+
+    public void OnConfig(View view) {
+        Toast toast = Toast.makeText(this,"設定",Toast.LENGTH_SHORT);
+        toast.show();
+        goToConfigPage();
+    }
+
+    private void goToConfigPage() {
+        Intent intent;
+        intent = new Intent(getApplicationContext(),ConfigActivity.class);
+        startActivity(intent);
+    }
+}
